eb2faec877717518e6c594069c954e6f0150ff42
mterrera29 <matiasterrera29@gmail.com>
2025-10-22 15:11:59 -0300

    medicos desde LocalStorage, subir imágenes en base64

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,56 +1,56 @@
-  document.addEventListener('DOMContentLoaded', function () {
-    const usuarioLogueado = sessionStorage.getItem('usuarioLogueado');
-    const usuarioPermiso = sessionStorage.getItem('permiso');
+document.addEventListener('DOMContentLoaded', function () {
+  const usuarioLogueado = sessionStorage.getItem('usuarioLogueado');
+  const usuarioPermiso = sessionStorage.getItem('permiso');
 
-    const elementosPrivados = document.querySelectorAll('.solo-logueado');
-    const elementosAdmin = document.querySelectorAll('.solo-admin');
-    const elementosPublicos = document.querySelectorAll('.solo-publico');
+  const elementosPrivados = document.querySelectorAll('.solo-logueado');
+  const elementosAdmin = document.querySelectorAll('.solo-admin');
+  const elementosPublicos = document.querySelectorAll('.solo-publico');
 
-    if (usuarioLogueado) {
-      elementosPrivados.forEach((el) => (el.style.display = 'block'));
-      elementosPublicos.forEach((el) => (el.style.display = 'none'));
+  if (usuarioLogueado) {
+    elementosPrivados.forEach((el) => (el.style.display = 'block'));
+    elementosPublicos.forEach((el) => (el.style.display = 'none'));
 
-      const nombreUsuario = document.getElementById('nombreUsuario');
-      if (nombreUsuario) {
-        nombreUsuario.textContent = usuarioLogueado;
-      }
-    } else {
-      elementosPrivados.forEach((el) => (el.style.display = 'none'));
-      elementosPublicos.forEach((el) => (el.style.display = 'block'));
+    const nombreUsuario = document.getElementById('nombreUsuario');
+    if (nombreUsuario) {
+      nombreUsuario.textContent = usuarioLogueado;
     }
+  } else {
+    elementosPrivados.forEach((el) => (el.style.display = 'none'));
+    elementosPublicos.forEach((el) => (el.style.display = 'block'));
+  }
 
-    if (usuarioPermiso === 'admin') {
-      elementosAdmin.forEach((el) => (el.style.display = 'block'));
-    } else {
-      elementosAdmin.forEach((el) => (el.style.display = 'none'));
-    }
-  });
+  if (usuarioPermiso === 'admin') {
+    elementosAdmin.forEach((el) => (el.style.display = 'block'));
+  } else {
+    elementosAdmin.forEach((el) => (el.style.display = 'none'));
+  }
+});
 
-  function actualizarNavbar() {
-    const usuarioLogueado = sessionStorage.getItem('usuarioLogueado');
-    const botonNavbar = document.querySelector('.navbar-text');
-    const welcomeUser = document.getElementById('welcomeUser');
+function actualizarNavbar() {
+  const usuarioLogueado = sessionStorage.getItem('usuarioLogueado');
+  const botonNavbar = document.querySelector('.navbar-text');
+  const welcomeUser = document.getElementById('welcomeUser');
 
-    if (usuarioLogueado) {
-      botonNavbar.innerHTML = `
+  if (usuarioLogueado) {
+    botonNavbar.innerHTML = `
       <a href="login.html" class="buttonNav" style="color: white" onclick="cerrarSesion()"
         >Salir</a
       >
       `;
-      welcomeUser.innerHTML = `
+    /* welcomeUser.innerHTML = `
       <span style="color: white">${usuarioLogueado}</span>
-      `;
-    } else {
-      botonNavbar.innerHTML = `
+      `; */
+  } else {
+    botonNavbar.innerHTML = `
         <a href="login.html" class="buttonNav" style="color: white">Entrar</a>
       `;
-    }
   }
+}
 
-  function cerrarSesion() {
-    sessionStorage.removeItem('usuarioLogueado');
-    sessionStorage.removeItem('permiso');
-    window.location.href = 'index.html';
-  }
+function cerrarSesion() {
+  sessionStorage.removeItem('usuarioLogueado');
+  sessionStorage.removeItem('permiso');
+  window.location.href = 'index.html';
+}
 
-  actualizarNavbar();
+actualizarNavbar();
